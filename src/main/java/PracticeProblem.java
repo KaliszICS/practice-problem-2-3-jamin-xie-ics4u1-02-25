36e015c0bb28e5e7b1a128ccaa20c517a2595b58
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-04-04 02:46:08 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,23 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	public static void bubbleSortString(String[] strings) {
+		int n = strings.length;
+		boolean swapped;
 
-	public static void q2() {
-		//Write question 2 code here
+		for (int i = 0; i < n - 1; i++) {
+			swapped = false;
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (strings[j].compareToIgnoreCase(strings[j + 1]) > 0) {
+					String temp = strings[j];
+					strings[j] = strings[j + 1];
+					strings[j + 1] = temp;
+					swapped = true;
+				}
+			}
+			if (!swapped) {
+				break;
+			}
+		}
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
